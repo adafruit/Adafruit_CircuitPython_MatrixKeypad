49b12b4a6ce49ede9799625a013585c75f3c891d
ladyada <limor@ladyada.net>
2018-05-28 21:49:12 -0400

    working code & example

--- adafruit_matrixkeypad.py
+++ adafruit_matrixkeypad.py
@@ -47,6 +47,45 @@ Implementation Notes
 """
 
 # imports
+import time
+from digitalio import DigitalInOut, Direction, Pull
+from micropython import const
+
 
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_MatrixKeypad.git"
+
+class Matrix_Keypad(object):
+    def __init__(self, row_pins, col_pins, keys):
+        if len(keys) != len(row_pins):
+            raise RuntimeError("Key name matrix doesn't match # of colums")
+        for row in keys:
+            if len(row) != len(col_pins):
+                raise RuntimeError("Key name matrix doesn't match # of rows")
+        self.row_pins = row_pins
+        self.col_pins = col_pins
+        self.keys = keys
+
+    @property
+    def pressed_keys(self):
+
+        # make a list of all the keys that are detected
+        pressed = []
+        
+        # set all pins pins to be inputs w/pullups
+        for pin in self.row_pins+self.col_pins:
+            pin.direction = Direction.INPUT
+            pin.pull = Pull.UP
+
+        for row in range(len(self.row_pins)):
+            # set one row low at a time
+            self.row_pins[row].direction = Direction.OUTPUT
+            self.row_pins[row].value = False
+            # check the column pins, which ones are pulled down
+            for col in range(len(self.col_pins)):
+                if not self.col_pins[col].value:
+                    pressed.append(self.keys[row][col])
+            # reset the pin to be an input
+            self.row_pins[row].direction = Direction.INPUT
+            self.row_pins[row].pull = Pull.UP
+        return pressed
---
+++ examples/matrixkeypad_simpletest.py
@@ -0,0 +1,20 @@
+import adafruit_matrixkeypad
+from digitalio import DigitalInOut, Direction, Pull
+import board
+import time
+
+# Classic 3x4 matrix keypad
+cols = [DigitalInOut(x) for x in (board.D2, board.D0, board.D4)]
+rows = [DigitalInOut(x) for x in (board.D1, board.D6, board.D5, board.D3)]
+keys = ((1, 2, 3),
+        (4, 5, 6),
+        (7, 8, 9),
+        ('*', 0, '#'))
+        
+keypad = adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keys)
+
+while True:
+    keys = keypad.pressed_keys
+    if keys:
+        print("Pressed: ", keys)
+    time.sleep(0.1)
