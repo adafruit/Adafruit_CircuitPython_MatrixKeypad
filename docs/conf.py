ecc2f7cabbfac19908b8713bcdeb75d13f9dcf32
Kattni <kattni@adafruit.com>
2018-06-04 18:02:50 -0400

    Updated automock

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@ extensions = [
 # digitalio, micropython and busio. List the modules you use. Without it, the
 # autodoc module docs will fail to generate with a warning.
 # autodoc_mock_imports = ["digitalio", "busio"]
+autodoc_mock_imports = ["digitalio"]
 
 
 intersphinx_mapping = {'python': ('https://docs.python.org/3.4', None),'BusDevice': ('https://circuitpython.readthedocs.io/projects/busdevice/en/latest/', None),'Register': ('https://circuitpython.readthedocs.io/projects/register/en/latest/', None),'CircuitPython': ('https://circuitpython.readthedocs.io/en/latest/', None)}
