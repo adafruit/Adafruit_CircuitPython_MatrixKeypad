6ff8b0deeaa86ac611bba2e87aae7504c531a1c2
Kattni <kattni@adafruit.com>
2018-06-04 18:56:28 -0400

    Sphinx update to docstring

--- adafruit_matrixkeypad.py
+++ adafruit_matrixkeypad.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 """
-`adafruit_MatrixKeypad`
+`adafruit_matrixkeypad`
 ====================================================
 
 CircuitPython library for matrix keypads
