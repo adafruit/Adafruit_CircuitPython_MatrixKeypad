a60cbe6c9e4b14ff26119276219f4fbf50c11cc7
Kattni <kattni@adafruit.com>
2018-06-04 17:51:14 -0400

    linting

--- examples/matrixkeypad_4x4.py
+++ examples/matrixkeypad_4x4.py
@@ -1,7 +1,7 @@
-import adafruit_matrixkeypad
-from digitalio import DigitalInOut, Direction, Pull
-import board
 import time
+import adafruit_matrixkeypad
+from digitalio import DigitalInOut
+import board
 
 # Extended 4x4 matrix keypad
 cols = [DigitalInOut(x) for x in (board.D0, board.D1, board.D2, board.D3)]
@@ -10,7 +10,7 @@ keys = ((1, 2, 3, 'A'),
         (4, 5, 6, 'B'),
         (7, 8, 9, 'C'),
         ('*', 0, '#', 'D'))
-        
+
 keypad = adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keys)
 
 while True:
