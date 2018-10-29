12d277c226594276627439253772f7cc949c2e50
Brennen Bearnes <brennen@adafruit.com>
2018-10-29 16:07:27 -0600

    trim comment length for pylint

--- examples/rpi_matrixkeypad_simpletest.py
+++ examples/rpi_matrixkeypad_simpletest.py
@@ -3,11 +3,13 @@ import digitalio
 import board
 import adafruit_matrixkeypad
 
-# Membrane 3x4 matrix keypad on Raspberry Pi - https://www.adafruit.com/product/419
+# Membrane 3x4 matrix keypad on Raspberry Pi -
+# https://www.adafruit.com/product/419
 cols = [digitalio.DigitalInOut(x) for x in (board.D26, board.D20, board.D21)]
 rows = [digitalio.DigitalInOut(x) for x in (board.D5, board.D6, board.D13, board.D19)]
 
-# 3x4 matrix keypad on Raspberry Pi - Rows and columns are mixed up for https://www.adafruit.com/product/3845
+# 3x4 matrix keypad on Raspberry Pi -
+# rows and columns are mixed up for https://www.adafruit.com/product/3845
 # cols = [digitalio.DigitalInOut(x) for x in (board.D13, board.D5, board.D26)]
 # rows = [digitalio.DigitalInOut(x) for x in (board.D6, board.D21, board.D20, board.D19)]
 
