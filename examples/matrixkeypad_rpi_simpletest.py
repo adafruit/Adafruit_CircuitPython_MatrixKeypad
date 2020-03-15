095aab9fa795a43f4b41e33c9081ead8ec5eb470
dherrada <dylan.herrada@gmail.com>
2020-03-15 17:21:14 -0400

    Ran black, updated to pylint 2.x

--- examples/matrixkeypad_rpi_simpletest.py
+++ examples/matrixkeypad_rpi_simpletest.py
@@ -13,10 +13,7 @@ rows = [digitalio.DigitalInOut(x) for x in (board.D5, board.D6, board.D13, board
 # cols = [digitalio.DigitalInOut(x) for x in (board.D13, board.D5, board.D26)]
 # rows = [digitalio.DigitalInOut(x) for x in (board.D6, board.D21, board.D20, board.D19)]
 
-keys = ((1, 2, 3),
-        (4, 5, 6),
-        (7, 8, 9),
-        ('*', 0, '#'))
+keys = ((1, 2, 3), (4, 5, 6), (7, 8, 9), ("*", 0, "#"))
 
 keypad = adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keys)
 
