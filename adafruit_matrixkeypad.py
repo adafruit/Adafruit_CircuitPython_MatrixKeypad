653dc23d997cfdff5e55ddcd7fe517d3eb95390e
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:13 -0400

    Update version string

--- adafruit_matrixkeypad.py
+++ adafruit_matrixkeypad.py
@@ -27,7 +27,7 @@ Implementation Notes
 # imports
 from digitalio import Direction, Pull
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_MatrixKeypad.git"
 
 
